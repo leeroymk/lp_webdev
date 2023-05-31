ef518fba7e930902758a95dc4059df4f5c4a9bf8
leeroymk <nikekursk@yandex.ru>
2023-05-31 11:14:10 +0300

    learing flask

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from python_org_news import get_python_news
 from weather import weather_by_city
 
 
@@ -7,11 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = 'Новости Python'
     weather = weather_by_city('Kursk, Russia')
-    if weather:
-        return f"Погода {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-    else:
-        return 'Сервис погоды временно недоступен'
+    news_list = get_python_news()
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
 
 
 if __name__ == '__main__':
--- weather.py
+++ weather.py
@@ -1,25 +1,30 @@
 import requests
+from settings import API_KEY
 
 
 def weather_by_city(city_name):
     weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
     params = {
-        'key': 'ab8805213f5a48a7bf7101102232305',
+        'key': API_KEY,
         'q': city_name,
         'format': 'json',
         'num_of_days': 1,
         'lang': 'ru',
     }
 
-    result = requests.get(weather_url, params=params)
-
-    weather = result.json()
-    if 'data' in weather:
-        if 'current_condition' in weather['data']:
-            try:
-                return weather['data']['current_condition'][0]
-            except (IndexError, TypeError):
-                return False
+    try:
+        result = requests.get(weather_url, params=params)
+        result.raise_for_status()
+        weather = result.json()
+        if 'data' in weather:
+            if 'current_condition' in weather['data']:
+                try:
+                    return weather['data']['current_condition'][0]
+                except (IndexError, TypeError):
+                    return False
+    except (requests.RequestException, ValueError):
+        print('Сетевая ошибка')
+        return False
     return False
 
 
